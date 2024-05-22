504cd3563f00e8f37544d2b139b7a1def6dbfaf3
Aliyah-W08 <aliyahwaris278@gmail.com>
2024-05-22 15:47:00 +0100

    secondCommit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import WeatherApp from './weatherApp/WeatherApp';
 
-const api = {
-  key: "409f6514aa06f9f298afd768741df7c3",
-  base: "https://home.openweathermap.org/api_keys"
-}
+
 
 
 
@@ -13,12 +10,6 @@ const App = () => {
     <div>
       <h1>Weather App</h1>
       <h2><WeatherApp /></h2>
-      {/* location */}
-      <p>Manchester, United Kingdom</p>
-      {/* temp */}
-      <p>18°C</p>
-      {/* weather */}
-      <p>Raining</p>
     </div>
   )
 }
--- src/weatherApp/WeatherApp.js
+++ src/weatherApp/WeatherApp.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react"
 
 const WeatherApp = () => {
     const [weather, setWeather] = useState("")
+    const [apiResult, setapiResult] = useState({})
 
     const handleSubmit = (e) => { // stops the browser refreshing after okay is pressed on the alert
         e.preventDefault();
@@ -9,28 +10,35 @@ const WeatherApp = () => {
         alert("Retrieving the weather for you ..."); // show a popup with whatever was inputted
     }
 
+    const api = {
+        key: "409f6514aa06f9f298afd768741df7c3",
+        base: "https://home.openweathermap.org/api_keys"
+    }
+
+    const searchPressed = () => {
+        fetch(`api.openweathermap.org/data/2.5/forecast?${weather}id=524901&appid=${api.key}`)
+            .then(response => response.json())
+            .then((response) => {
+                setapiResult = response
+            })
+
+    }
+
+
     return (
         <div id="container">
-            <form onSubmit={handleSubmit}>
-                <label>
+            <div id="inputandbutton">
+                <form onSubmit={handleSubmit}>
                     <input type="text"
-                        value={weather}
                         onChange={(e) => setWeather(e.target.value)}
                         placeholder="Enter your city/town" />
-                </label>
-                <button onClick={searchPressed} className="btn" type="submit">Check Weather</button>
-            </form>
+                    <button onClick={searchPressed} className="btn" type="submit">Check Weather</button>
+                </form>
+                <p>{apiResult.name}</p>
+            </div>
+            <p>{apiResult.name}</p>
         </div>
     )
 }
-
-
-// const WeatherApp = () => {
-//     return(
-//         <div>
-//             <h1>/</h1>
-//         </div>
-//     )
-// }
 
 export default WeatherApp
